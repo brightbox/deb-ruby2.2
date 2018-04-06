be82bbef420f0974d415bd9f10fde4b589615b82
John Leach <john@johnleach.co.uk>
2018-04-06 18:22:12 +0100

    New upstream version 2.2.10

--- lib/tmpdir.rb
+++ lib/tmpdir.rb
@@ -1,7 +1,7 @@
 #
 # tmpdir - retrieve temporary directory path
 #
-# $Id: tmpdir.rb 48905 2014-12-20 08:58:07Z akr $
+# $Id: tmpdir.rb 63017 2018-03-28 14:34:14Z usa $
 #
 
 require 'fileutils'
@@ -108,8 +108,10 @@ class Dir
     def make_tmpname((prefix, suffix), n)
       prefix = (String.try_convert(prefix) or
                 raise ArgumentError, "unexpected prefix: #{prefix.inspect}")
+      prefix = prefix.delete("#{File::SEPARATOR}#{File::ALT_SEPARATOR}")
       suffix &&= (String.try_convert(suffix) or
                   raise ArgumentError, "unexpected suffix: #{suffix.inspect}")
+      suffix &&= suffix.delete("#{File::SEPARATOR}#{File::ALT_SEPARATOR}")
       t = Time.now.strftime("%Y%m%d")
       path = "#{prefix}#{t}-#{$$}-#{rand(0x100000000).to_s(36)}"
       path << "-#{n}" if n
